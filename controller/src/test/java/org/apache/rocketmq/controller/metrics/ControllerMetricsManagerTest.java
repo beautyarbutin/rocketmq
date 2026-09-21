e17e5c8bd8b23cbaac33487b02a3a64ba00c9d7d
beautyarbutin <169455703+beautyarbutin@users.noreply.github.com>
2026-09-21 17:35:14 +0800

    [ISSUE #10833] fix(metrics): preserve colons in config values

---
+++ controller/src/test/java/org/apache/rocketmq/controller/metrics/ControllerMetricsManagerTest.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.controller.metrics;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import org.apache.rocketmq.common.ControllerConfig;
+import org.apache.rocketmq.common.metrics.MetricsExporterType;
+import org.apache.rocketmq.controller.ControllerManager;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ControllerMetricsManagerTest {
+
+    @Test
+    public void testMetricsLabelValueContainingColon() {
+        ControllerConfig controllerConfig = new ControllerConfig();
+        controllerConfig.setControllerDLegerGroup("group");
+        controllerConfig.setControllerDLegerSelfId("n0");
+        controllerConfig.setControllerDLegerPeers("n0-127.0.0.1:9878");
+        controllerConfig.setMetricsExporterType(MetricsExporterType.LOG);
+        controllerConfig.setMetricsLabel("endpoint:https://collector:4317");
+
+        ControllerManager controllerManager = mock(ControllerManager.class);
+        when(controllerManager.getControllerConfig()).thenReturn(controllerConfig);
+
+        ControllerMetricsManager.getInstance(controllerManager);
+        Attributes attributes = ControllerMetricsManager.newAttributesBuilder().build();
+
+        assertThat(attributes.get(AttributeKey.stringKey("endpoint")))
+            .isEqualTo("https://collector:4317");
+    }
+}
---
+++ proxy/src/test/java/org/apache/rocketmq/proxy/metrics/ProxyMetricsManagerTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.proxy.metrics;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import org.apache.rocketmq.common.metrics.MetricsExporterType;
+import org.apache.rocketmq.proxy.config.ProxyConfig;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProxyMetricsManagerTest {
+
+    @Test
+    public void testMetricsLabelValueContainingColon() throws Exception {
+        ProxyConfig proxyConfig = new ProxyConfig();
+        proxyConfig.setMetricsExporterType(MetricsExporterType.LOG);
+        proxyConfig.setMetricsLabel("endpoint:https://collector:4317");
+
+        ProxyMetricsManager metricsManager = ProxyMetricsManager.initClusterMode(proxyConfig);
+        metricsManager.start();
+        try {
+            Attributes attributes = ProxyMetricsManager.newAttributesBuilder().build();
+            assertThat(attributes.get(AttributeKey.stringKey("endpoint")))
+                .isEqualTo("https://collector:4317");
+        } finally {
+            metricsManager.shutdown();
+        }
+    }
+}
