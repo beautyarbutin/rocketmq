14bfb0a3f223bd5286bcf3c34d6c838f6b8a08d1
beautyarbutin <169455703+beautyarbutin@users.noreply.github.com>
2026-09-21 13:54:09 +0800

    [ISSUE #10878] Fix tieredstore reads across file segments

--- tieredstore/src/main/java/org/apache/rocketmq/tieredstore/file/FlatAppendFile.java
+++ tieredstore/src/main/java/org/apache/rocketmq/tieredstore/file/FlatAppendFile.java
@@ -241,23 +241,33 @@ public class FlatAppendFile {
             }
         }
 
-        FileSegment fileSegment1 = fileSegmentList.get(index);
-        FileSegment fileSegment2 = offset + length > fileSegment1.getCommitOffset() &&
-            fileSegmentList.size() > index + 1 ? fileSegmentList.get(index + 1) : null;
-
-        if (fileSegment2 == null) {
-            return fileSegment1.readAsync(offset - fileSegment1.getBaseOffset(), length);
+        FileSegment fileSegment = fileSegmentList.get(index);
+        if (offset + length <= fileSegment.getCommitOffset() || fileSegmentList.size() <= index + 1) {
+            return fileSegment.readAsync(offset - fileSegment.getBaseOffset(), length);
         }
 
-        int segment1Length = (int) (fileSegment1.getCommitOffset() - offset);
-        return fileSegment1.readAsync(offset - fileSegment1.getBaseOffset(), segment1Length)
-            .thenCombine(fileSegment2.readAsync(0, length - segment1Length),
-                (buffer1, buffer2) -> {
-                    ByteBuffer buffer = ByteBuffer.allocate(buffer1.remaining() + buffer2.remaining());
-                    buffer.put(buffer1).put(buffer2);
-                    buffer.flip();
-                    return buffer;
-                });
+        List<CompletableFuture<ByteBuffer>> readFutures = new ArrayList<>();
+        long readOffset = offset;
+        int remainingLength = length;
+        for (; index < fileSegmentList.size() && remainingLength > 0; index++) {
+            fileSegment = fileSegmentList.get(index);
+            int readLength = remainingLength;
+            if (index < fileSegmentList.size() - 1) {
+                readLength = (int) Math.min(remainingLength, fileSegment.getCommitOffset() - readOffset);
+            }
+            readFutures.add(fileSegment.readAsync(readOffset - fileSegment.getBaseOffset(), readLength));
+            readOffset += readLength;
+            remainingLength -= readLength;
+        }
+
+        CompletableFuture<?>[] futures = readFutures.toArray(new CompletableFuture<?>[0]);
+        return CompletableFuture.allOf(futures).thenApply(nil -> {
+            int resultLength = readFutures.stream().mapToInt(future -> future.join().remaining()).sum();
+            ByteBuffer result = ByteBuffer.allocate(resultLength);
+            readFutures.forEach(future -> result.put(future.join()));
+            result.flip();
+            return result;
+        });
     }
 
     public void shutdown() {
